c43443ee75f11c2cbe2e7bd1438102e0d09d3d33
Beau Raines <beau.raines@gmail.com>
2014-08-21 15:06:29 -0700

    Process follows command submits a job to the queue

--- app/commands/ProcessFollows.php
+++ app/commands/ProcessFollows.php
@@ -40,12 +40,21 @@ class ProcessFollows extends Command {
 	 */
 	public function fire()
 	{
-		$user_ids = Follow::where('status_code',108)->distinct()->get(['user_id']);
+		$user_ids = Follow::where('status_code','<>',1)  // Successful follow
+                                    ->where('status_code','<>',0)  // Already follow
+                                    ->where('status_code','<>',160) // Already requested to follow
+                                    ->where('status_code','<>',162) // Blocked from following this user
+                                    ->where('status_code','<>',108) // User not found
+                                    ->where('status_code','<>',162) // Blocked from following this user 
+                                    ->orWhere('status_code',NULL) // Not followed yet
+				    ->distinct()->get(['user_id']);
 
 		foreach ( $user_ids as $user) {
 
-		  $this->info($user['user_id']);
 		  // submit the job to the queue for $user'user_id']
+                $job =Queue::push('FollowService', ['user_id' => $user['user_id']]);
+		  $this->info('Started job ' . $job . ' for user ' .$user['user_id']);
+
 
 		}
 		
