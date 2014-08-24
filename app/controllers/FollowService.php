0479d7184f15abbe292d7d2a5a28aee326439f67
Beau Raines <beau.raines@gmail.com>
2014-08-24 15:42:53 -0700

    Adding job completion email

--- app/controllers/FollowService.php
+++ app/controllers/FollowService.php
@@ -17,6 +17,19 @@ class FollowService {
 
                 $follows = new Follow;
                 $follows = $follows->followFromFollow(NULL, $friends, $toa);
+		
+		$user = User::find(Auth::user()->id);
+
+		$name = $user->name;
+		$email = $user->email;
+		$twitter_user = $user->twitter_user;
+		$data = ['name' => $user->name, 'email' => $user->email, 'twitter_user' => $user->twitter_user, $follows => $follows];
+		$subject = 'Job ' . $job . ' complete for ' .$name;
+		
+		Mail::queue('emails.jobcomplete', $data, function($message) use ($name, $email, $twitter_user, $follows)
+		{
+    		  $message->to('beau.raines@gmail.com', 'Beau Raines')->subject($subject);
+		});
 
 		$job->delete();
 
---
+++ app/views/emails/jobcomplete.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		Job {{ $job }} complete for {{ $name }}<br>
+		{{ $follows }} were processed.
+
+		Have a nice day!
+			
+
+		</div>
+	</body>
+</html>
